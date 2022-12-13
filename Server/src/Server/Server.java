38d05db31615f986269c32dadeaebce21b82c1ee
lqonkee81 <lqonkee81@yandex.ru>
2022-12-13 23:32:12 +0300

    Add logging

--- Server/src/Server/Server.java
+++ Server/src/Server/Server.java
@@ -1,9 +1,14 @@
 package Server;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import Package.*;
 
@@ -12,6 +17,8 @@ public class Server {
 
     private ArrayList<ClientHandler> allClients = new ArrayList<ClientHandler>();
     private ServerSocket sSoket;
+
+    private final String PATH_TO_LOGS_DIRECTORY = "./Server/logs/";
 
     public Server() {
         try {
@@ -42,8 +49,33 @@ public class Server {
     }
 
     public void sendMessageToAllUsers(Message msg) {
+        logging(msg);
         for (ClientHandler client : allClients) {
             client.sendMessage(msg);
+        }
+    }
+
+    private void logging(Message msg) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        Date date = new Date(System.currentTimeMillis());
+
+        String pathLogFile = PATH_TO_LOGS_DIRECTORY + "log_" + formatter.format(date) + ".log";
+
+        File f = new File(pathLogFile);
+
+        try {
+            if (f.createNewFile()) {
+                System.out.println("SERVER DEBUG: Created new log file: " + pathLogFile);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (FileWriter fileWriter = new FileWriter(pathLogFile, true)) {
+            fileWriter.write(msg.toString() + "\n");
+        } catch (FileNotFoundException e) {
+
+        } catch (IOException e) {
         }
     }
 }
